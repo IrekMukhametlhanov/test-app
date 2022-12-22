ea50fb70f7cd309f90566f8c1392d384ca4be51f
IrekMukhametlhanov <selfon1@mail.ru>
2022-12-23 00:08:01 +0300

    two

--- src/Components/RepoCard.tsx
+++ src/Components/RepoCard.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useActions } from '../hooks/actions'
+import { useAppSelector } from '../hooks/redux'
 import { IRepo } from '../type/type'
 
 const RepoCard = ({repo}:{repo: IRepo}) => {
+
+
+  const {addFavorite, deleteFavorite}= useActions()
+
+  const {favourites} = useAppSelector(state => state.github)
+  
+  const [isFav, setIsFav]=useState(favourites.includes(repo.html_url))
+
+
+   const addToFavorite = (event: React.MouseEvent<HTMLButtonElement>) =>{
+     event.preventDefault()
+
+     addFavorite(repo.html_url)
+     setIsFav(true)
+   }
+
+   const delToFavorite = (event: React.MouseEvent<HTMLButtonElement>) =>{
+    event.preventDefault()
+    deleteFavorite(repo.html_url)
+    setIsFav(false)
+   }
+
+
   return (
     <div className='border py-3 px-5 rounded mb-2 hover:shadow-md hover: bg-gray-100 transition-all'>
         <a href={repo.html_url} target="_blank">
@@ -11,6 +36,12 @@ const RepoCard = ({repo}:{repo: IRepo}) => {
         Wathcers: <span className='font-bold'>{repo.watchers}</span>
         </p>
         <p className='text-sm font-thin'>{repo?.description}</p>
+        {!isFav && <button className='py-2 px-3 bg-yellow-400 rounded hover: shadow-md transition-all'
+        onClick={addToFavorite}
+        >Add</button>}
+         {isFav && <button className='py-2 px-3 bg-yellow-400 rounded hover: shadow-md transition-all'
+        onClick={delToFavorite}
+        >remove</button>}
         </a>
     </div>
 
--- src/Pages/FavoritesPages.tsx
+++ src/Pages/FavoritesPages.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useAppSelector } from '../hooks/redux'
 
 export const FavoritesPages = () => {
+
+  const {favourites} = useAppSelector(state => state.github)
+
+  if (favourites.length === 0) return <p className='text-center'>нет избранных</p>
+
   return (
-    <div>FavoritesPages</div>
+    <ul className='list-none'>
+        {favourites.map(f => (
+          <li key={f}>
+            <a href={f}>{f}</a>
+          </li>
+        )
+        )}
+    </ul>
+
   )
 }
